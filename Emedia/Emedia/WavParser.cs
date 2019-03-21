773706c6b3daa32f1bd2c6181b8042fd971e77f8
Norbert Cyran <cyran.norbert97@gmail.com>
2019-03-21 22:10:13 +0100

    Create WavParser extracting data segment from wav file

---
+++ Emedia/Emedia/WavParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Emedia
+{
+    public class WavParser
+    {
+        private readonly string wavFilePath;
+        public const int WavHeaderOffset = 44;
+
+        public WavParser(string wavFilePath) => this.wavFilePath = wavFilePath;
+
+        public byte[] GetWavData()
+        {
+            using (var reader = new BinaryReader(File.OpenRead(wavFilePath)))
+            {
+                reader.BaseStream.Seek(WavHeaderOffset, SeekOrigin.Begin);
+                return reader.ReadBytes((int)reader.BaseStream.Length - WavHeaderOffset);
+            }
+        }
+    }
+}
