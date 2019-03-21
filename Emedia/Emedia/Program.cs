9a5795112c855a9e42b1cd9b3c21200769ae7391
Norbert Cyran <cyran.norbert97@gmail.com>
2019-03-21 22:10:57 +0100

    Add main

---
+++ Emedia/Emedia/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Emedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            string filePath = Path.Combine(projectDirectory, "11k16bitpcm.wav");
+            if (File.Exists(filePath))
+            {
+                var parser = new WavParser(filePath);
+                byte[] wavData = parser.GetWavData();
+                Console.WriteLine(BitConverter.ToString(wavData));
+            }
+            else
+            {
+                Console.WriteLine("File not found.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
