bedf167ef99ea30f013ad20b66d18d179f136de2
robij1996 <robert.stanik1996@gmail.com>
2019-04-05 09:03:26 +0200

    E-MEDIA

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Controls;
+using System.Drawing;
+
+
+
 
 namespace Lab1
 {
@@ -10,6 +18,31 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
+
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            string filePath = Path.Combine(projectDirectory, @"C:\Users\48888\Desktop\E-media\Lab1\11k16bitpcm.wav");
+            if (File.Exists(filePath))
+            {
+                var parser = new WavParser(filePath);
+                byte[] wavData = parser.GetWavData();
+                //Console.WriteLine(BitConverter.ToString(wavData));
+
+
+                
+                var header = parser.Metadata();
+                Console.WriteLine(BitConverter.ToString(header));
+
+                string h = System.Text.Encoding.ASCII.GetString(header);
+                Console.WriteLine(h);
+            }
+            else
+            {
+                Console.WriteLine("File not found.");
+            }
+    Console.ReadLine();
         }
+
+
     }
 }
---
+++ Lab1/WavParser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Lab1
+{
+    public class WavParser
+    {
+        private readonly string wavFilePath;
+        public const int WavHeaderOffset = 44;
+
+        public WavParser(string wavFilePath) => this.wavFilePath = wavFilePath;
+
+        public byte[] GetWavData()
+        {
+            using (var reader = new BinaryReader(File.OpenRead(wavFilePath)))
+            {
+                reader.BaseStream.Seek(WavHeaderOffset, SeekOrigin.Begin);
+                return reader.ReadBytes((int)reader.BaseStream.Length - WavHeaderOffset);
+            }
+        }
+
+        public byte[] Metadata()
+        {
+            var reader = new BinaryReader(File.OpenRead(wavFilePath));
+            byte[] header = reader.ReadBytes(WavHeaderOffset);
+            return header;
+        }
+
+    }
+
+}
