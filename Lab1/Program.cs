dfe47a70980b5edce5915f6f5a71464646c40b92
robij1996 <robert.stanik1996@gmail.com>
2019-04-10 16:51:22 +0200

    Dodanie rozdzielenia na nagłówki i generowanie klucza

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -18,28 +18,29 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-
+            RSA e = new RSA();
             string workingDirectory = Environment.CurrentDirectory;
             string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
             string filePath = Path.Combine(projectDirectory, @"C:\Users\48888\Desktop\E-media\Lab1\11k16bitpcm.wav");
             if (File.Exists(filePath))
             {
                 var parser = new WavParser(filePath);
-                byte[] wavData = parser.GetWavData();
-                //Console.WriteLine(BitConverter.ToString(wavData));
-
-
-                Console.WriteLine(wavData.Length);
-                var header = parser.Metadata();
-                parser.WriteData(header);
-                
-
+                e.data = parser.GetWavData();
+                e.header = parser.Metadata();
+                //parser.WriteData(e.header);
             }
             else
             {
                 Console.WriteLine("File not found.");
             }
-    Console.ReadLine();
+            e.GetKeys(13, 11);
+            
+
+
+
+
+            Console.ReadLine();
+
         }
 
 
---
+++ Lab1/RSA.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class RSA
+    {
+        public byte[] header;
+        public byte[] data;
+
+        public int[] publicKey = new int[2];
+        public int[] privateKey = new int[2];
+
+        public void GetKeys(int Prime1, int Prime2)
+        {
+            int e; 
+            int phi = (Prime1 - 1) * (Prime2 - 1);
+            int n = Prime2 * Prime1;
+            for (e = 7 ; Nwd(e, phi) != 1; e += 2);
+            int d = Odwr_mod(e, phi);
+
+            this.privateKey[0] = d;
+            this.privateKey[1] = n;
+            this.publicKey[0] = e;
+            this.publicKey[1] = n;
+
+        }
+
+        public int Nwd(int a, int b)
+        {
+            int t;
+            while (b != 0)
+            {
+                t = b;
+                b = a % b;
+                a = t;
+            }
+            return a;
+        }
+
+        int Odwr_mod(int a, int n)
+        {
+            int a0, n0, p0, p1, q, r, t;
+
+            p0 = 0; p1 = 1; a0 = a; n0 = n;
+            q = n0 / a0;
+            r = n0 % a0;
+            while (r > 0)
+            {
+                t = p0 - q * p1;
+                if (t >= 0)
+                    t = t % n;
+                else
+                    t = n - ((-t) % n);
+                p0 = p1; p1 = t;
+                n0 = a0; a0 = r;
+                q = n0 / a0;
+                r = n0 % a0;
+            }
+            return p1;
+        }
+
+    }
+}
